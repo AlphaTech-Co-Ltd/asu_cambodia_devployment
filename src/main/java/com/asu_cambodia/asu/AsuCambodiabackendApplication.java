3347d2ed02d3e61084d0e44d7cf87594896744c9
SkyHeng168 <sreangtongheng009@gmail.com>
2025-08-18 15:23:52 +0700

    fix error

--- src/main/java/com/asu_cambodia/asu/AsuCambodiabackendApplication.java
+++ src/main/java/com/asu_cambodia/asu/AsuCambodiabackendApplication.java
@@ -8,16 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AsuCambodiabackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
 
-        System.setProperty("spring.datasource.url", dotenv.get("DATASOURCE_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("DATASOURCE_USER"));
-        System.setProperty("spring.datasource.password", dotenv.get("DATASOURCE_PASSWORD"));
-        System.setProperty("FRONTEND_URL", dotenv.get("FRONTEND_URL"));
-        System.setProperty("jwt.secret", dotenv.get("JWT_SECRETKEY"));
-
+        // Load environment variables directly from system (works on Railway and Docker)
+        System.setProperty("spring.datasource.url", System.getenv("DATASOURCE_URL"));
+        System.setProperty("spring.datasource.username", System.getenv("DATASOURCE_USER"));
+        System.setProperty("spring.datasource.password", System.getenv("DATASOURCE_PASSWORD"));
+        System.setProperty("FRONTEND_URL", System.getenv("FRONTEND_URL"));
+        System.setProperty("jwt.secret", System.getenv("JWT_SECRETKEY"));
 
         SpringApplication.run(AsuCambodiabackendApplication.class, args);
     }
 
 }
+
