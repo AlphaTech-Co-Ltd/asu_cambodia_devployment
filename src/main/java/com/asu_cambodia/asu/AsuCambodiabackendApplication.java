845e5db1c23e95f34417572ffb50d22541f08f8f
SkyHeng168 <sreangtongheng009@gmail.com>
2025-08-18 18:53:26 +0700

    fix error

--- src/main/java/com/asu_cambodia/asu/AsuCambodiabackendApplication.java
+++ src/main/java/com/asu_cambodia/asu/AsuCambodiabackendApplication.java
@@ -1,6 +1,5 @@
 package com.asu_cambodia.asu;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
