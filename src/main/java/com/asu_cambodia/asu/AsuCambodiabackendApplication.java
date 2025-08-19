1a331c73916850e6696197c7a19904564a37083e
SkyHeng168 <sreangtongheng009@gmail.com>
2025-08-19 10:21:32 +0700

    devployment

--- src/main/java/com/asu_cambodia/asu/AsuCambodiabackendApplication.java
+++ src/main/java/com/asu_cambodia/asu/AsuCambodiabackendApplication.java
@@ -1,6 +1,5 @@
 package com.asu_cambodia.asu;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AsuCambodiabackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        // Set environment variables for Spring to read
-        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-        System.setProperty("JWT", dotenv.get("JWT"));
-
         SpringApplication.run(AsuCambodiabackendApplication.class, args);
     }
 }
