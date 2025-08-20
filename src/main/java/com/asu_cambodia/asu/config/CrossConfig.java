3ce651e8008001377036d5295e3ea32da9a36a77
SkyHeng168 <sreangtongheng009@gmail.com>
2025-08-20 13:25:42 +0700

    deploy Complete

--- src/main/java/com/asu_cambodia/asu/config/CrossConfig.java
+++ src/main/java/com/asu_cambodia/asu/config/CrossConfig.java
@@ -1,5 +1,6 @@
 package com.asu_cambodia.asu.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CrossConfig {
 
+    @Value("${frontend.url}") // inject from properties/env
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer crossConfigConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
